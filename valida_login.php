d867abe3c7e8a354d8a089b8bcd1472acf285d69
Kaique Rodrigues Ramos <Kaiqueramospessoal@gmail.com>
2020-04-28 00:19:33 -0300

    Add Auth for Login and Senha

--- valida_login.php
+++ valida_login.php
@@ -1,17 +1,24 @@
 <?php
+    //variável para validar login
+    $validador = false;
     //Usuários válidos
     $usuarios_app = array(
         array('email' => 'adm_teste@email.com', 'senha' => '123456'),
         array('email' => 'user@email.com', 'senha' => '987654'),
     );
-    echo '<pre>';
-    print_r($usuarios_app);
-    echo '</pre>';
+    foreach($usuarios_app as $user){
+        
+        if($user['email'] == $_POST["email"] && $user['senha'] == $_POST["senha"]){
+            $validador = true;
+        }
+    }
+    if($validador){
+        echo 'Autenticando... <br>';
+        echo 'Usuário válido, logando...';
+        }else{
+        header('Location: index.php?login=erro');
+        }
+    
 
-    print_r($_POST);
-    echo '<br>';
-    echo $_POST["email"];
-    echo '<br>';
-    echo $_POST["senha"];
 
 ?>
